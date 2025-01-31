77c32beebc626cc45c576c3f7f97c2014c328993
MarkSian <merksianipar@gmail.com>
2025-01-30 18:07:57 -0700

    updated package.json

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -29,16 +29,18 @@ const SavedCandidates = () => {
         ) : (
             <ul className="list bg-base-100 rounded-box shadow-md">
               {savedCandidates.map((candidate, index) => (
-                    <li key={candidate.username} className="list-row">
+                <li key={candidate.username} className="list-row">
                     <div className="text-4xl font-thin opacity-30 tabular-nums">{index + 1}</div>
-                    <div><img className = "size-20 rounded-box" src={candidate.avatar_url} alt={`${candidate.name}'s avatar`}/></div>
+                    <div>
+                        <img className = "size-20 rounded-box" src={candidate.avatar_url} alt={`${candidate.name}'s avatar`}/>
+                    </div>
                     <div className="list-col-grow">
-                    <div className="text-xl uppercase font-semibold tracking-wide">Username: {candidate.username|| "Username Not Available"}</div>
-                    <div className="text-xs uppercase font-semibold mt-3 tracking-wide ">Name: {candidate.name || "Name Not Available"}</div>
-                    <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Location: {candidate.location || "Location Not Available"}</div>
-                    <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Company: {candidate.company || "Company Not Available"}</div>
-                    <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Email: {candidate.email || "Email Not Available"}</div>
-                    <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Github: {candidate.html_url || "URL Not Available"}</div>
+                        <div className="text-xl uppercase font-semibold tracking-wide">Username: {candidate.username|| "Username Not Available"}</div>
+                        <div className="text-xs uppercase font-semibold mt-3 tracking-wide ">Name: {candidate.name || "Name Not Available"}</div>
+                        <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Location: {candidate.location || "Location Not Available"}</div>
+                        <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Company: {candidate.company || "Company Not Available"}</div>
+                        <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Email: {candidate.email || "Email Not Available"}</div>
+                        <div className="text-xs uppercase font-semibold mt-1 tracking-wide">Github: {candidate.html_url || "URL Not Available"}</div>
                     </div>
                     <button className="btn btn-warning" onClick={() => handleRejectedCandidate(candidate.username)}>Reject</button>
                 </li>
